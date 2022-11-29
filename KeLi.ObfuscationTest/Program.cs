206b6363d174a2dd6a158d363dd39c99aade0fc6
KeLi <kelilive@outlook.com>
2022-11-30 00:25:50 +0800

    Initial commit

---
+++ KeLi.ObfuscationTest/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace KeLi.ObfuscationTest
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            Test();
+        }
+
+        private static void Test()
+        {
+            for (var i = 0; i < 100; i++)
+            {
+                if (i < 10)
+                    Console.WriteLine(i + 1);
+
+                else
+                {
+                    if (i > 80)
+                        Console.WriteLine(i - 2);
+
+                    if (i > 90)
+                        Console.WriteLine(i - 5);
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
